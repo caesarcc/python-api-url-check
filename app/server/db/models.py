b9bd3f95cb4028a46b3f754bc9a6943360249c3b
Caesar C. Cesar <caesarcesar.rs@gmail.com>
2022-04-30 14:19:32 -0300

    ajustes no pre-processamento

--- app/server/db/models.py
+++ app/server/db/models.py
@@ -1,5 +1,6 @@
 
 import string
+import uuid
 from datetime import datetime
 from random import choices
 
@@ -8,25 +9,16 @@ from .extensions import db
 
 class Noticia(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(5), unique=True)
-    url = db.Column(db.String(512))
+    uuid = db.Column(db.String(16), unique=True)
+    url = db.Column(db.String(512), nullable=True)
     resumo = db.Column(db.Text(), nullable=True)
     #visitas = db.Column(db.Integer, default=0)
     data_acesso = db.Column(db.DateTime, default=datetime.now)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.uuid = self.generate_uuid()
-
-    def generate_uuid(self):
-        characters = string.digits + string.ascii_letters
-        uuid = ''.join(choices(characters, k=5))
-        url = self.query.filter_by(uuid=uuid).first()
-
-        if url:
-            return self.generate_uuid()
-
-        return uuid
+        self.uuid = uuid.uuid4()
+        #url = self.query.filter_by(uuid=uuid).first()
 
     def __repr__(self):
         return f'{self.url}, {self.uuid}'
