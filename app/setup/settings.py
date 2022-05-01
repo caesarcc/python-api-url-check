a28cc6eb5820e3253ee952c97eedd7f5a7bf7651
Caesar C. Cesar <caesarcesar.rs@gmail.com>
2022-05-01 16:48:43 -0300

    atualização

--- app/setup/settings.py
+++ app/setup/settings.py
@@ -1,4 +1,9 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///site.db"
+basedir = os.path.dirname(__file__)
+rootdir, basedir = os.path.split(basedir)
+while basedir != 'app':
+    rootdir, basedir = os.path.split(rootdir)
+dblocaldir = os.path.join(rootdir, 'dados\\fakenews_check.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite:///" + dblocaldir
 SQLALCHEMY_TRACK_MODIFICATIONS = False
